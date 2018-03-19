0df74c95bc5bbfb267aaf8b13736f723ff2aca6a
Paul Buss <cyberserfer@gmailcom>
2018-03-19 07:56:22 -0500

    inital pages created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+import {
+  Materials,
+  Style,
+  Genre,
+  Year,
+  Home
+} from './components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+
+            <h1 className="App-title">Paul Buss - Art Portfolio</h1>
+          </header>
+          <ul className="navMenu">
+            <li className=""><NavLink to="/">Home</NavLink></li>
+            <li className=""><NavLink to="/materials">Materials</NavLink></li>
+            <li className=""><NavLink to="/style">Style</NavLink></li>
+            <li className=""><NavLink to="/genre">Genre</NavLink></li>
+            <li className=""><NavLink to="/year">Year</NavLink></li>
+          </ul>
+          <Switch>
+          <Route exact path="/materials" component={Materials} />
+          <Route path="/style" component={Style} />
+          <Route path="/genre" component={Genre} />
+          <Route path="/year" component={Year} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
