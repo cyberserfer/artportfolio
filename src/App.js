90782b3c5339d06c36b6f217aad2b1c0422ca56d
Cary <cary.wolff@swarmingtech.com>
2018-03-22 20:31:29 -0500

    blueprint and styled-components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Navbar, NavbarGroup, Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
 import {
   BrowserRouter as Router,
-  Link,
   Route,
+  Link,
   NavLink,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom';
-import {
-  Materials,
-  Style,
-  Genre,
-  Year,
-  Home
-} from './components';
+import Materials from './components/Materials';
+import Style from './components/Style';
+import Genre from './components/Genre';
+import Year from './components/Year';
+import Home from './components/Home';
+
+
+const Header = styled.div`
+    width: 100%;
+    padding: 10px 20px;
+    background-color: #666;
+    h1 {
+        color: #fff;
+    }
+`;
+
+const StyledNavbarGroup = styled(NavbarGroup)`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+        flex: 1;
+        text-align: center;
+    }
+`;
+
+const MaterialsDropDown = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 200px;
+    height: 250px;
+    a {
+        font-size: 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        flex: 1;
+        &:hover {
+            background: #ccc;
+        }
+    }
+`;
 
 class App extends Component {
-  render() {
-    return (
+    render() {
+      return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-
-            <h1 className="App-title">Paul Buss - Art Portfolio</h1>
-          </header>
-          <ul className="navMenu">
-            <li className=""><NavLink to="/">Home</NavLink></li>
-            <li className=""><NavLink to="/materials">Materials</NavLink></li>
-            <li className=""><NavLink to="/style">Style</NavLink></li>
-            <li className=""><NavLink to="/genre">Genre</NavLink></li>
-            <li className=""><NavLink to="/year">Year</NavLink></li>
-          </ul>
-          <Switch>
-          <Route exact path="/materials" component={Materials} />
+          <div>
+          <Header>
+            <h1>Paul Buss - Art Portfolio</h1>
+        </Header>
+        <Navbar>
+            <StyledNavbarGroup>
+                <NavLink to='/'>Home</NavLink>
+                <Popover minimal interactionKind={PopoverInteractionKind.HOVER} position={Position.DOWN} content={
+                    <MaterialsDropDown>
+                        <Link to='/materials/wood'>Wood</Link>
+                        <Link to='/materials/metal'>Metal</Link>
+                        <Link to='/materials/glass'>Glass</Link>
+                        <Link to='/materials/paper'>Paper</Link>
+                    </MaterialsDropDown>
+                    } target={<NavLink to='/materials'>Materials</NavLink>} />
+                <NavLink to='/style'>Style</NavLink>
+                <NavLink to='/genre'>Genre</NavLink>
+                <NavLink to='/year'>Year</NavLink>
+            </StyledNavbarGroup>
+        </Navbar>
+        <Switch>
+          <Route path="/materials" component={Materials} />
           <Route path="/style" component={Style} />
           <Route path="/genre" component={Genre} />
           <Route path="/year" component={Year} />
-          <Route exact path="/" component={Home} />
+          <Route path="/" component={Home} />
+          <Redirect from='*' to='/' />
         </Switch>
-        </div>
-      </Router>
+    </div>
+    </Router>
     );
   }
 }
---
+++ src/components/Materials.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Intro = styled.div`
+    
+`;
+
+class Materials extends Component {
+    render() {
+        return (
+            <div>
+                <Intro>Stuff</Intro>
+            </div>
+        )
+    } 
+}
+
+export default Materials;
